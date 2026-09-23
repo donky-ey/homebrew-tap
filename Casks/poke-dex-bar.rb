87716e524cbd038d2afd2e1a3bc2cc44d8fa2640
donky ey <leedg020831@gmail.com>
2026-09-23 10:25:55 +0900

    cask: poke-dex-bar 1.18.2

--- Casks/poke-dex-bar.rb
+++ Casks/poke-dex-bar.rb
@@ -1,21 +1,15 @@
 cask "poke-dex-bar" do
-  version "1.18.1"
-  sha256 :no_check
+  version "1.18.2"
+  sha256 "adc15011835896a1d6c6908f3d51d9990ffad67d1d69019ae5f781a3ecab693a"
 
   url "https://github.com/donky-ey/PokeDexBar/releases/download/v#{version}/PokeDexBar.zip"
   name "PokeDexBar"
   desc "Menu bar app turning AI coding token usage into a Pokemon collection"
   homepage "https://github.com/donky-ey/PokeDexBar"
 
-  depends_on macos: ">= :sonoma"
+  depends_on macos: :sonoma
 
   app "PokeDexBar.app"
-
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args: ["-cr", "#{appdir}/PokeDexBar.app"],
-                   sudo: false
-  end
 
   zap trash: [
     "~/Library/Application Support/PokeDexBar",
